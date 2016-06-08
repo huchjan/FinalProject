c6f9647ce94f0beff40638767d2f260f755806c7
huchjan <janethuchteman@gmail.com>
2016-06-08 15:54:31 -0600

    June8_4pm

--- app.js
+++ app.js
@@ -33,6 +33,34 @@ app.get('/projects', function (req, res) {
 
 //This is the route to the project page
 //:slug is a variable
+
+app.get("/annuals/:slug", function (req, res) {
+    //get the project data based on the requested slug
+    var projectData = getProjectBySlug(req.params.slug);
+    //render the "projects.handlebars" template using the project data
+    res.render('project', {
+        project: projectData
+    });
+});
+
+app.get("/brochures/:slug", function (req, res) {
+    //get the project data based on the requested slug
+    var projectData = getProjectBySlug(req.params.slug);
+    //render the "projects.handlebars" template using the project data
+    res.render('project', {
+        project: projectData
+    });
+});
+
+app.get("/digital/:slug", function (req, res) {
+    //get the project data based on the requested slug
+    var projectData = getProjectBySlug(req.params.slug);
+    //render the "projects.handlebars" template using the project data
+    res.render('project', {
+        project: projectData
+    });
+});
+
 app.get("/tradeshows/:slug", function (req, res) {
     //get the project data based on the requested slug
     var projectData = getProjectBySlug(req.params.slug);
@@ -43,15 +71,6 @@ app.get("/tradeshows/:slug", function (req, res) {
 });
 
 app.get("/misc/:slug", function (req, res) {
-    //get the project data based on the requested slug
-    var projectData = getProjectBySlug(req.params.slug);
-    //render the "projects.handlebars" template using the project data
-    res.render('project', {
-        project: projectData
-    });
-});
-
-app.get("/brochures/:slug", function (req, res) {
     //get the project data based on the requested slug
     var projectData = getProjectBySlug(req.params.slug);
     //render the "projects.handlebars" template using the project data
@@ -78,14 +97,6 @@ app.get("/logos/:slug", function (req, res) {
     });
 });
 
-app.get("/annuals/:slug", function (req, res) {
-    //get the project data based on the requested slug
-    var projectData = getProjectBySlug(req.params.slug);
-    //render the "projects.handlebars" template using the project data
-    res.render('project', {
-        project: projectData
-    });
-});
 
 //define(function (require) {
 //Define the header and footer partials for the Handlebars
@@ -110,21 +121,20 @@ function getProjectBySlug(slug) {
 var projects = [
     {
         slug: "WPX_annual",
-        name: "2013 WPX Energy Annual Report",
+        name: "2013 WPX Energy annual report",
         image: "WPX_Annual_Report2013.jpg",
         desc: "10k wrap for WPX Energy&rsquo;s annual report to its shareholders. Book was perfect bound and printed 4-color process for the wrap and one color on the 10k.",
         cat: "ANNUAL REPORTS",
-        left: "wpx_tradeshow",
-        right: "../brochures/annual_enrollment",
-        small_images: ["WPX_AR_Inside.jpg"]
+        left: "../tradeshows/wpx_tradeshow",
+        right: "../brochures/annual_enrollment"
     },
     {
         slug: "annual_enrollment",
-        name: "Williams Benefits Brochure",
+        name: "Williams benefits brochure",
         image: "Health_Care_Broch.jpg",
         desc: "Part of Williams&rsquo; annual benefits enrollment rollout, this brochure was designed to let employees and retirees know how to get the most from their healthcare benefits.",
         cat: "BROCHURES",
-        left: "WPX_annual",
+        left: "../annuals/WPX_annual",
         right: "campfire_broch"
     },
     {
@@ -134,6 +144,15 @@ var projects = [
         desc: "Pro bono tri-fold brochure created for Camp Fire USA while serving on the Green Country Council&rsquo;s PR Committee.",
         cat: "BROCHURES",
         left: "annual_enrollment",
+        right: "../digital/digital_graph"
+    },
+    {
+        slug: "digital_graph",
+        name: "WPX website graphic",
+        image: "BoostingOurBbl_graph.jpg",
+        desc: "Digital image showing WPX&rsquo;s oil production. Graph was created for the company&rsquo;s external website.",
+        cat: "DIGITAL",
+        left: "../brochures/campfire_broch",
         right: "../invitations/worldchallenge_invite"
     },
     {
@@ -142,25 +161,25 @@ var projects = [
         image: "World_Challenge_Invite.jpg",
         desc: "Invitation and custom golf ball sleeve created for the Williams World Challenge. Williams partnered with the Tiger Woods Foundation, which benefitted underprivileged kids, and was a major sponsor for several years.",
         cat: "INVITATIONS",
-        left: "campfire_broch",
-        right: "nutcracker_invite"
+        left: "../digital/digital_graph",
+        right: "nwp_invite"
     },
     {
-        slug: "nutcracker_invite",
-        name: "Williams Nutcracker Market invitation",
-        image: "NutcrackerMktInvite.jpg",
-        desc: "Evite for a Williams Midstream customer event in Houston.",
+        slug: "nwp_invite",
+        name: "Northwest Pipeline anniversary invitation",
+        image: "NWP_Invite.jpg",
+        desc: "Postcard invitation for Northwest Pipeline&rsquo;s 50th anniversary event, which included an antique car show.",
         cat: "INVITATIONS",
         left: "worldchallenge_invite",
         right: "rigmosaic_invite"
     },
     {
         slug: "rigmosaic_invite",
-        name: "Rig Mosaic invitation",
+        name: "Rig mosaic invitation",
         image: "RigMosaicInvite.jpg",
-        desc: "Cover of an invitation created for the unveiling of a mosaic commissioned by Williams.",
+        desc: "Cover of an invitation created for a reception to honor the legacy of the energy industry in Tulsa with the unveiling of a glass mosaic mural commissioned by Williams.",
         cat: "INVITATIONS",
-        left: "nutcracker_invite",
+        left: "nwp_invite",
         right: "../logos/stpats5k_logos"
     },
     {
@@ -188,7 +207,7 @@ var projects = [
         desc: "Logo designed for WPX Energy.",
         cat: "LOGOS",
         left: "pacific_logos",
-        right: "../misc/mlk_tshirt"
+        right: "mlk_tshirt"
     },
     {
         slug: "mlk_tshirt",
@@ -196,34 +215,43 @@ var projects = [
         image: "mlk_illus.png",
         desc: "Design created for a sweatshirt given to Williams employees participating in Tulsa&rsquo;s Martin Luther King Day parade.",
         cat: "MISCELLANEOUS",
-        left: "minuteman_logos",
+        left: "../logos/minuteman_logos",
         right: "wpx_tshirt"
     },
     {
         slug: "wpx_tshirt",
-        name: "WPX Family Picnic Tee",
+        name: "WPX Family Picnic tee",
         image: "WPX_Picnic_Tee.jpg",
         desc: "T-shirt designed for WPX employees for a family picnic at the Drillers&rsquo; ONEOK Field in downtown Tulsa, Oklahoma.",
         cat: "MISCELLANEOUS",
         left: "mlk_tshirt",
+        right: "wecu_flyer"
+    },
+    {
+        slug: "wecu_flyer",
+        name: "Williams Employees Credit Union flyer and postcard",
+        image: "WECU_flyer.jpg",
+        desc: "Flyer and postcard created to promote WECU&rsquo;s Home Equity Line of Credit.",
+        cat: "MISCELLANEOUS",
+        left: "wpx_tshirt",
         right: "wpx_vehiclewrap"
     },
     {
         slug: "wpx_vehiclewrap",
-        name: "WPX Vehicle Wrap",
+        name: "WPX vehicle wrap",
         image: "Vehicle_Wrap.jpg",
         desc: "When the WPX brand was launched in 2012, I designed the wrap for the company&rsquo;s trucks that ran on compressed natural gas.",
         cat: "MISCELLANEOUS",
-        left: "wpx_tshirt",
+        left: "wecu_flyer",
         right: "../tradeshows/wpx_tradeshow"
     },
     {
         slug: "wpx_tradeshow",
-        name: "Williams Tradeshow Display",
+        name: "Williams tradeshow display",
         image: "Tradeshow1.jpg",
         desc: "Pop-up display created for Williams&rsquo; Midstream group to use at industry tradeshows. It needed to be a small, quick-to-assemble display that accommodated a tv monitor.",
         cat: "TRADE SHOWS",
-        left: "wpx_vehiclewrap",
+        left: "../misc/wpx_vehiclewrap",
         right: "../annuals/WPX_annual"
     }
 ];
